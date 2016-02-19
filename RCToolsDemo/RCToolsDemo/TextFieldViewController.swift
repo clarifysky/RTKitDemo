e3c5b07301a51037da4c5beb94ddbcdc92563a4f
gitHomeCL <cl0067@126.com>
2016-02-19 09:37:48 +0800

    add a sample of change return button of keyboard

--- RCToolsDemo/RCToolsDemo/TextFieldViewController.swift
+++ RCToolsDemo/RCToolsDemo/TextFieldViewController.swift
@@ -11,12 +11,23 @@ import UIKit
 class TextFieldViewController: UIViewController {
 
     @IBOutlet weak var textFieldTest: UITextField!
+    var textFieldReturn: RCTextField?
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.title = "textFieldTest"
         self.textFieldTest.delegate = self
+        self.textFieldTest.returnKeyType = UIReturnKeyType.Send
+        
+//        self.textFieldReturn = RCTextField(frame: CGRectMake(10, 150, 200, 40))
+//        self.textFieldReturn?.doneTitle = "send"
+//        self.textFieldReturn?.doneAction = {
+//            println("You clicked send button in keyboard view")
+//        }
+//        self.textFieldReturn?.layer.borderColor = UIColor.greenColor().CGColor
+//        self.textFieldReturn?.layer.borderWidth = 1.0
+//        self.view.addSubview(self.textFieldReturn!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +45,11 @@ extension TextFieldViewController: UITextFieldDelegate {
         println("textFromTextField: \(textField.text)")
         let testStr = textField.text.stringByReplacingCharactersInRange(range.toRange(textField.text), withString: string)
         println(testStr + ", length: \(count(testStr))")
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        println("you pressed the return button")
         return true
     }
 }
