9057b5a7c7c18bc8800049f296650d36e7cf9c1c
clwm01 <cl0067@126.com>
2016-05-17 14:26:14 +0800

    extend NSLayoutConstraint

--- RCToolsDemo/RTView.swift
+++ RCToolsDemo/RTView.swift
@@ -475,3 +475,11 @@ extension UIColor {
         return UIColor(red: CGFloat(r / 255.0), green: CGFloat(g / 255.0), blue:CGFloat(b / 255.0), alpha: CGFloat(alpha))
     }
 }
+
+
+extension NSLayoutConstraint {
+    override public var description: String {
+        let id = identifier ?? ""
+        return "id: \(id), constant: \(constant)"
+    }
+}
